cbea9e0b3145f6b2cb39d326d01909db4c058829
sourabh <sourabh@kali.sourabh>
2026-06-07 18:27:39 +0530

    add user model and auth architecture

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,22 @@
 import app from "./app.js";
 import dotenv from "dotenv"
+import connectDB from "./config/db.js";
 
 dotenv.config()
 
 let port = process.env.PORT || 5000
 
-app.listen(port,() => {
-    console.log("server has started")
-})
+const startDB = async ( ) => {
+    try{
+       await connectDB()
+       app.listen(port, () => {
+      console.log(`server running on port ${port}`)
+    })
+    }
+    catch(error){
+        console.log(error)
+        process.exit(1)
+    }
+}
+
+startDB()
